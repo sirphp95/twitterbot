040ed401c74a387dce698ef1be08f2f66482a325
Pedro <pedro.nis.ph@gmail.com>
2023-04-29 19:20:10 -0300

    Update seguir.py

--- twitterBot/seguir.py
+++ twitterBot/seguir.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key = 'MbDaxA5ch7L9EoXEM5OT1exg8'
-consumer_secret = 'cbnEJa9h9SNo788qmIxZbh3TUcECBOa5F4sovehS6DzBy3derd'
-access_token = '1652374140932837386-QIIsGbqGB3UcvisuYkZSnadZnEVWst'
-access_token_secret = 't8opFixJ5JK5LTxoNNrrfl7Uszji2Qg2kSLJwLBzQcXhb'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
